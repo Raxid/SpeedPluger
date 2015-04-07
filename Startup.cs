e100e8c1e1c14fc630afcbad9acb147f187b6286
Raxid <radoslav.stenke@gmail.com>
2015-04-07 12:14:41 +0200

    7.4.15
    Task 0

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpeedPluger.Startup))]
+namespace SpeedPluger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
